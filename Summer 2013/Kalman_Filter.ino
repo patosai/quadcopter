78f70e686fbecbaad9e3d47e41686292a951ab9c
Patrick Tsai <patosai@live.com>
2014-05-28 19:35:09 -0400

    Cleaned up master

--- Summer 2013/Kalman_Filter.ino
+++
@@ -1,6 +0,0 @@
-void getKalmanAngles()
-{
-  pitch = kalmanX.getAngle(thetaX, gy, time2) + 3.32;
-  roll = kalmanY.getAngle(thetaY, gx, time2) + 0.86;
-}
-
--- Summer 2013/Sensor Code/README.md
+++
@@ -1,5 +0,0 @@
-Readme
-==========
-
-This is the sensor only code for the quadcopter!
-It displays sensor data in the serial monitor, 9600bd.
